37a2edee948539196c58bdeb246df4b210fa42a1
Brent Pendergraft <bpent3l@gmail.com>
2015-04-20 13:42:49 -0500

    Initial Commit

---
+++ iMoose/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iMoose
+//
+//  Created by Brent Pendergraft on 4/20/15.
+//  Copyright (c) 2015 Bpenderg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
